8734ab1e5d346eab03b185978b9059becaf3bfa3
Danilo Machuca <danilo.machuca.dev@gmail.com>
2025-09-24 20:26:43 -0300

    tentativa2

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { useEffect } from "react";
 import { Inter } from "next/font/google";
 
--- src/components/SocialMediaHeaderList/index.tsx
+++ src/components/SocialMediaHeaderList/index.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Link from "next/link";
 
 import { FaWhatsapp, FaGithub, FaLinkedin, FaGoogle } from "react-icons/fa";

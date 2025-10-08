4e05f55cc59eb6ee1075881cf1a6d01e017a7b8f
Danilo Machuca <danilo.machuca.dev@gmail.com>
2025-10-08 19:34:53 -0300

    fix: aos

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -2,8 +2,18 @@
 
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import Aos from "aos";
+import "aos/dist/aos.css";
+
+import { useEffect } from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
+  useEffect(() => {
+    Aos.init();
+    window.scrollTo({
+      top: 0,
+    });
+  }, []);
   return (
     <>
       <Header />
